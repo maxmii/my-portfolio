27357782526c7a4658b0c497a1ecc7f14d20100d
Max Morris <morrm046@gmail.com>
2024-02-08 23:00:45 +0000

    Add data for my portfolio in data.ts

---
+++ src/lib/data.ts
@@ -0,0 +1,62 @@
+import React from 'react';
+import { CgWorkAlt } from 'react-icons/cg';
+import { FaReact } from 'react-icons/fa';
+import { LuGraduationCap } from 'react-icons/lu';
+
+export const links = [
+  {
+    name: 'Home',
+    hash: '#home',
+  },
+  {
+    name: 'About',
+    hash: '#about',
+  },
+  {
+    name: 'Projects',
+    hash: '#projects',
+  },
+  {
+    name: 'Skills',
+    hash: '#skills',
+  },
+  {
+    name: 'Experience',
+    hash: '#experience',
+  },
+  {
+    name: 'Contact',
+    hash: '#contact',
+  },
+] as const;
+
+export const experiencesData = [] as const;
+
+export const projectsData = [] as const;
+
+export const skillsData = [
+  'HTML',
+  'CSS',
+  'Bootstrap',
+  'Linux',
+  'Drone CI',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Next.js',
+  'Node.js',
+  'Git',
+  'Tailwind',
+  'MongoDB',
+  'Redux',
+  'Express',
+  'PostgreSQL',
+  'C#',
+  'ASP.NET',
+  'AWS',
+  'Docker',
+  'Kubernetes',
+  'Python',
+  'Django',
+  'Framer Motion',
+] as const;
